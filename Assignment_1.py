62c76a410e5e3048813ef1ad553de03b8c54842c
MattDoolin <matthew.doolin@duke.edu>
2015-01-27 21:36:59 -0500

    Assignment 1
    Import and Extract Data

---
+++ Assignment_1.py
@@ -0,0 +1,20 @@
+#Initialize Script
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+#Establishing File Paths
+main_dir = "C:\Users\Matt\Documents\Nicholas School-2nd Year\Spring 2015\Big Data Analysis\Python Codes"
+txt_file = "\Data\Assignment_1_data.txt"
+
+df = pd.read_table(main_dir + txt_file, sep = " ") #indicating delimiter is a space
+df = pd.read_table(main_dir + txt_file, sep = "/s") #does the same thing
+
+list(df)
+
+##Extracting Rows 60-99
+df[60:100]
+
+#Extracting rows where kwh's are greater than 30
+k = df.kwh
+df[k > 30]
