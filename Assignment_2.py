cba3c1b63d665f19d2c5598ac4f8b6bf9aa609da
MattDoolin <matthew.doolin@duke.edu>
2015-02-04 16:57:36 -0500

    Assignment 2-Final
    Corrections to Assignment 2

--- Assignment_2.py
+++ Assignment_2.py
@@ -15,21 +15,26 @@ df
 #Converting missing values to NaN--(-) is 
 missing = [".", "NA", 'NULL', '', '-']
 df = pd.read_csv(os.path.join(main_dir, csv_file), na_values = missing)
-#Drops any duplicated full rows
+#Drops any duplicated FULL rows
 df1 = df.drop_duplicates()
 
 empty_rows = df1['consump'].isnull()
 #shows the subset of rows that have a missing consump value
+#same as below
+df1[df1.consump.isnull()]
 df1[empty_rows]
-
+#Creates dataframe that is probably same as df1 since none has been dropped
 df2 = df1[empty_rows]
 df2
 
-duplicate_date = df1.duplicated(subset = ['panid','date'])
-df1[duplicate_date]
-df3 = df1[duplicate_date]
+##Need to look at top to bottom and bottom to top in order to gauge where missing values are
+df1[df1.duplicated(subset = ['panid', 'date'])]
+df1[df1.duplicated(subset = ['panid','date'], take_last = True)]
 
-df4 = df1.drop(duplicate_date)
+#Creating new dataframe that drops the duplicated panid/date rows with missing consump values
+df3 = df1.drop_duplicates(['panid','date'], take_last = True)
+df3[df3.duplicated(['panid', 'date'])]
+
 ##mean of consump variable after dropping fully duplicated rows and 
 #rows where date is duplicated and consump value is missing
-df4['consump'].mean()
+df3.consump.mean()
