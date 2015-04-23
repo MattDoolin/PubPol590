c7a8ea009e6a70ef6a90ea86a8b20e46bdc6f090
MattDoolin <matthew.doolin@duke.edu>
2015-04-22 23:53:06 -0400

    Final Project Info

--- Doolin_section_1.py
+++ Doolin_section_1.py
@@ -1,3 +1,9 @@
+######################################################################
+#PUBPOL 590-Big Data Analysis
+#Final Project
+#Section 1
+######################################################################
+
 from __future__ import division
 import pandas as pd
 from pandas import Series, DataFrame
@@ -37,15 +43,42 @@ print df.kwh.mean()
 #Part B
 ################################################
 
+#importing additional data
 allocation = pd.read_csv(root + 'residential_allocations.csv', usecols = [0,1])
-timeseries = pd.read_csv(root + 'time_correction.csv')
+timeseries = pd.read_csv(root + 'time_correction.csv', parsedates = 0)
 
+#creating hour_cer column to merge on
 df['hour_cer'] = df['date_cer'] % 100
 
-df1 = pd.merge(df, allocation, timeseries)
+#merging datasets
+df = pd.merge(df, allocation)
+df = pd.merge(df, timeseries)
 
+print "\n\n\n"
+print df(df.ID == 1021).head(20)
 
+##################################################
+#Part C
+##################################################
 
+#Aggregating by month
+grp = df.groupby(['year', 'month', 'ID', 'allocation'])
+df1 = grp['kwh'].sum().reset_index()
+df1['kwh_mo'] = 'kwh_' + df1['month'].apply(str)
 
+#pivoting from long to wide
+df_piv = df1.pivot('ID', 'kwh_mo', 'kwh')
+df_piv.reset_index(inplace = True)
+df_piv.columns.name = None
+
+df_piv
+
+print "\n\n\n"
+print df_piv.shape
+print df_piv.head()
+
+###################################################
+#Part D
+###################################################
 
 
